73e02c4ae7f9618377384d028c0b5ccf2d894571
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2016-10-17 16:41:33 +0200

    Add marshmallow-jsonapi

--- web/app/common/resources/v1.py
+++ web/app/common/resources/v1.py
@@ -1,6 +1,9 @@
 from flask import Flask, Blueprint
 import flask_restful
 
+from marshmallow_jsonapi import Schema, fields
+from marshmallow import validate
+
 API_VERSION_V1 = 1
 API_VERSION = API_VERSION_V1
 
@@ -8,11 +11,20 @@ example_api_v1_bp = Blueprint('api_v1', __name__)
 example_api_v1 = flask_restful.Api(example_api_v1_bp)
 
 
+class HelloWorldSchema(Schema):
+    id = fields.Str(dump_only=True)
+    hello = fields.String()
+    version = fields.Integer()
+
+    class Meta:
+        type_ = 'hello_world'
+
+
 class HelloWorld(flask_restful.Resource):
     def get(self):
-        return {
-            'hello': 'world',
-            'version': API_VERSION,
-        }
+        api_info = dict(hello='world', version=API_VERSION)
+        schema = HelloWorldSchema()
+        result = schema.dump(api_info)
+        return result
 
 example_api_v1.add_resource(HelloWorld, '/hello')
