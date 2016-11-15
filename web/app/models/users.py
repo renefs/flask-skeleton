c3c3fc941d06c4db9a0aa7c701a2e92190825be1
René Fernández <renefs87@gmail.com>
2016-11-15 20:26:01 +0100

    Add tablename to models

--- web/app/models/users.py
+++ web/app/models/users.py
@@ -4,11 +4,18 @@ from flask_dance.consumer.backend.sqla import OAuthConsumerMixin
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    __table_args__ = (db.CheckConstraint('email = lower(email)', 'lowercase_email'),)
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(256), unique=True)
-    # ... other columns as needed
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
 
 
 class OAuth(db.Model, OAuthConsumerMixin):
+    __tablename__ = 'flask_dance_oauth'
+
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
     user = db.relationship(User)
