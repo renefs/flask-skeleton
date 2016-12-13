b8396a647894b8f53a8460e2ec7cac92cd98c705
René Fernández <renefs87@gmail.com>
2016-12-13 19:52:26 +0100

    Add style and main.js

--- web/app/public/js/main.js
+++ web/app/public/js/main.js
@@ -1,3 +1,13 @@
 /**
  * Created by renefernandez on 28/09/16.
  */
+$('.message .close')
+    .on('click', function () {
+        $(this)
+            .closest('.message')
+            .transition('fade');
+    });
+
+$("a.sidebar-toggle").click(function () {
+    $('.ui.sidebar').sidebar('toggle');
+});
